596435f2ae5f5666acd677ffb6f75e72e58b3dc8
fzyzcjy <ch271828n@outlook.com>
2025-06-22 10:06:02 +0800

    more

--- csrc/deep_ep.cpp
+++ csrc/deep_ep.cpp
@@ -1254,7 +1254,8 @@ Buffer::low_latency_combine(const torch::Tensor& x, const torch::Tensor& topk_id
 
 void Buffer::notify_src_signals(const torch::Tensor& src_signals, int index) {
     const uint32_t* addr = src_signals.data_ptr<uint32_t>() + index;
-    CU_CHECK(cuStreamWriteValue32(comm_stream, (CUdeviceptr) addr, 1, 0));
+    // TODO comm stream or current stream or whatever stream?
+    CU_CHECK(cuStreamWriteValue32(at::cuda::getCurrentCUDAStream(), (CUdeviceptr) addr, 1, 0));
 }
 
 torch::Tensor
