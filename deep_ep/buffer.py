4f8a2471cb1a4dc2e398da3bd09e647cbe17eb6d
Shangyan Zhou <sy.zhou@deepseek.com>
2025-06-25 12:28:29 +0800

    Fix style.

--- deep_ep/buffer.py
+++ deep_ep/buffer.py
@@ -151,11 +151,12 @@ class Buffer:
     def get_comm_stream(self) -> torch.Stream:
         """
         Get the communication stream.
-        
+
+        Returns:
+            stream: the communication stream. 
         """
         ts: torch.Stream = self.runtime.get_comm_stream()
         return torch.cuda.Stream(stream_id=ts.stream_id, device_index=ts.device_index, device_type=ts.device_type)
-
 
     def get_local_buffer_tensor(self, dtype: torch.dtype, size: Optional[torch.Size] = None,
                                 offset: int = 0, use_rdma_buffer: bool = False) -> torch.Tensor:
