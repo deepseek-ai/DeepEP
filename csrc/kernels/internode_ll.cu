6c3e56935768cbb2a8e69456162da98d64d5e518
fzyzcjy <ch271828n@outlook.com>
2025-06-26 09:49:56 +0800

    more

--- csrc/kernels/internode_ll.cu
+++ csrc/kernels/internode_ll.cu
@@ -109,8 +109,91 @@ dispatch(void* packed_recv_x, void* packed_recv_x_scales,
         const auto num_threads = (num_warps - 1) * 32;
         const size_t hidden_bf16_int4 = kHidden / kNumElemsPerRead;
 
-        for (int dst_local_expert_idx = 0; dst_local_expert_idx < num_local_experts; ++dst_expert_local_idx) {
+        for (int interesting_dst_local_expert_idx = 0; interesting_dst_local_expert_idx < num_local_experts; ++interesting_dst_local_expert_idx) {
+            for (int token_idx = sm_id; token_idx < num_tokens; token_idx += num_sms) {
+                const auto x_int4 = static_cast<const int4*>(x) + token_idx * hidden_bf16_int4;
+                const auto rdma_x_src_idx = reinterpret_cast<int*>(static_cast<uint8_t*>(rdma_x) + token_idx * num_bytes_per_msg);
+                const auto rdma_x_vec = reinterpret_cast<vec_t*>(reinterpret_cast<uint8_t*>(rdma_x_src_idx) + sizeof(int4));
+                const auto rdma_x_scales = reinterpret_cast<float*>(reinterpret_cast<uint8_t*>(rdma_x_vec) + hidden_bytes);
 
+                // Overlap top-k index read and source token index writes
+//                 auto dst_expert_idx = warp_id < num_topk ? static_cast<int>(__ldg(topk_idx + token_idx * num_topk + warp_id)) : -1;
+                auto dst_expert_idx = warp_id < num_topk ? TODO_load_from_smem : -1;
+                
+                // TODO maybe improve
+                if (dst_expert_idx != interesting_dst_local_expert_idx) {
+                    continue;
+                }
+
+                TODO; // not need to send this?
+                thread_id == 0 ? (*rdma_x_src_idx = token_idx) : 0;
+
+                // FP8 cast
+                #pragma unroll
+                for (int i = thread_id; i < hidden_bf16_int4; i += num_threads) {
+                    // Read
+                    auto int4_value = __ldg(x_int4 + i);
+
+                    if constexpr (kUseFP8) {
+                        // Calculate local amax
+                        auto bf16_values = reinterpret_cast<nv_bfloat16*>(&int4_value);
+                        float fp32_values[kNumElemsPerRead];
+                        float amax = kFP8Margin, scale, scale_inv;
+                        #pragma unroll
+                        for (int j = 0; j < kNumElemsPerRead; ++ j) {
+                            fp32_values[j] = static_cast<float>(bf16_values[j]);
+                            amax = fmaxf(amax, fabsf(fp32_values[j]));
+                        }
+
+                        // Reduce amax and scale
+                        EP_STATIC_ASSERT(kNumElemsPerRead * 32 / kNumPerChannels == 2, "Invalid vectorization");
+                        amax = half_warp_reduce_max(amax);
+                        calculate_fp8_scales(amax, scale, scale_inv, round_scale);
+                        if (lane_id == 0 or lane_id == 16)
+                            rdma_x_scales[i * kNumElemsPerRead / 128] = scale_inv;
+
+                        // Cast into send buffer
+                        vec_t int2_value;
+                        auto fp8x2_values = reinterpret_cast<__nv_fp8x2_storage_t*>(&int2_value);
+                        #pragma unroll
+                        for (int j = 0; j < kNumElemsPerRead; j += 2) {
+                            float2 fp32x2 = {fp32_values[j] * scale, fp32_values[j + 1] * scale};
+                            fp8x2_values[j / 2] = __nv_cvt_float2_to_fp8x2(fp32x2, __NV_SATFINITE, __NV_E4M3);
+                        }
+                        rdma_x_vec[i] = int2_value;
+                    } else {
+                        // Reinterpret-cast is for C++14 compatibility
+                        rdma_x_vec[i] = *reinterpret_cast<vec_t*>(&int4_value);
+                    }
+                }
+                asm volatile("bar.sync 1, %0;" :: "r"(num_threads));
+
+                // Issue IBGDA sends
+                if (dst_expert_idx >= 0) {
+                    int slot_idx = lane_id == 0 ? atomicAdd(atomic_counter_per_expert + dst_expert_idx, 1) : 0;
+                    slot_idx = __shfl_sync(0xffffffff, slot_idx, 0);
+                    const auto dst_rank = dst_expert_idx / num_local_experts;
+                    const auto dst_expert_local_idx = dst_expert_idx % num_local_experts;
+                    const auto src_ptr = reinterpret_cast<uint64_t>(rdma_x_src_idx);
+                    const auto dst_ptr = reinterpret_cast<uint64_t>(rdma_recv_x) +
+                                         dst_expert_local_idx * num_ranks * num_max_dispatch_tokens_per_rank * num_bytes_per_msg +
+                                         rank * num_max_dispatch_tokens_per_rank * num_bytes_per_msg +
+                                         slot_idx * num_bytes_per_msg;
+                    const auto dst_p2p_ptr = nvshmemi_get_p2p_ptr(dst_ptr, rank, dst_rank);
+                    if (dst_p2p_ptr == 0) {
+                        nvshmemi_ibgda_put_nbi_warp(dst_ptr, src_ptr, num_bytes_per_msg, dst_rank, dst_expert_local_idx, lane_id, slot_idx);
+                    } else {
+                        // NOTES: only 2 load iterations for 7K hidden with 8 unrolls
+                        const auto* src_int4_ptr = reinterpret_cast<const int4*>(src_ptr);
+                        const auto* dst_int4_ptr = reinterpret_cast<int4*>(dst_p2p_ptr);
+                        UNROLLED_WARP_COPY(8, lane_id, num_int4_per_msg, dst_int4_ptr, src_int4_ptr, ld_nc_global, st_na_global);
+                    }
+
+                    // Increase counter after finishing
+                    __syncwarp();
+                    lane_id == 0 ? atomic_add_release_global(atomic_finish_counter_per_expert + dst_expert_idx, 1) : 0;
+                }
+            }
         }
     }
 
@@ -118,82 +201,6 @@ dispatch(void* packed_recv_x, void* packed_recv_x_scales,
 //     // 1. The first-kind warps for FP8 cast and sending top-k tokens
 //     // 2. The last warp for reading `topk_idx` and count for per-expert information
 //     if (warp_id < num_warps - 1) {
-//         for (int token_idx = sm_id; token_idx < num_tokens; token_idx += num_sms) {
-//             const auto x_int4 = static_cast<const int4*>(x) + token_idx * hidden_bf16_int4;
-//             const auto rdma_x_src_idx = reinterpret_cast<int*>(static_cast<uint8_t*>(rdma_x) + token_idx * num_bytes_per_msg);
-//             const auto rdma_x_vec = reinterpret_cast<vec_t*>(reinterpret_cast<uint8_t*>(rdma_x_src_idx) + sizeof(int4));
-//             const auto rdma_x_scales = reinterpret_cast<float*>(reinterpret_cast<uint8_t*>(rdma_x_vec) + hidden_bytes);
-//
-//             // Overlap top-k index read and source token index writes
-//             auto dst_expert_idx = warp_id < num_topk ? static_cast<int>(__ldg(topk_idx + token_idx * num_topk + warp_id)) : -1;
-//             thread_id == 0 ? (*rdma_x_src_idx = token_idx) : 0;
-//
-//             // FP8 cast
-//             #pragma unroll
-//             for (int i = thread_id; i < hidden_bf16_int4; i += num_threads) {
-//                 // Read
-//                 auto int4_value = __ldg(x_int4 + i);
-//
-//                 if constexpr (kUseFP8) {
-//                     // Calculate local amax
-//                     auto bf16_values = reinterpret_cast<nv_bfloat16*>(&int4_value);
-//                     float fp32_values[kNumElemsPerRead];
-//                     float amax = kFP8Margin, scale, scale_inv;
-//                     #pragma unroll
-//                     for (int j = 0; j < kNumElemsPerRead; ++ j) {
-//                         fp32_values[j] = static_cast<float>(bf16_values[j]);
-//                         amax = fmaxf(amax, fabsf(fp32_values[j]));
-//                     }
-//
-//                     // Reduce amax and scale
-//                     EP_STATIC_ASSERT(kNumElemsPerRead * 32 / kNumPerChannels == 2, "Invalid vectorization");
-//                     amax = half_warp_reduce_max(amax);
-//                     calculate_fp8_scales(amax, scale, scale_inv, round_scale);
-//                     if (lane_id == 0 or lane_id == 16)
-//                         rdma_x_scales[i * kNumElemsPerRead / 128] = scale_inv;
-//
-//                     // Cast into send buffer
-//                     vec_t int2_value;
-//                     auto fp8x2_values = reinterpret_cast<__nv_fp8x2_storage_t*>(&int2_value);
-//                     #pragma unroll
-//                     for (int j = 0; j < kNumElemsPerRead; j += 2) {
-//                         float2 fp32x2 = {fp32_values[j] * scale, fp32_values[j + 1] * scale};
-//                         fp8x2_values[j / 2] = __nv_cvt_float2_to_fp8x2(fp32x2, __NV_SATFINITE, __NV_E4M3);
-//                     }
-//                     rdma_x_vec[i] = int2_value;
-//                 } else {
-//                     // Reinterpret-cast is for C++14 compatibility
-//                     rdma_x_vec[i] = *reinterpret_cast<vec_t*>(&int4_value);
-//                 }
-//             }
-//             asm volatile("bar.sync 1, %0;" :: "r"(num_threads));
-//
-//             // Issue IBGDA sends
-//             if (dst_expert_idx >= 0) {
-//                 int slot_idx = lane_id == 0 ? atomicAdd(atomic_counter_per_expert + dst_expert_idx, 1) : 0;
-//                 slot_idx = __shfl_sync(0xffffffff, slot_idx, 0);
-//                 const auto dst_rank = dst_expert_idx / num_local_experts;
-//                 const auto dst_expert_local_idx = dst_expert_idx % num_local_experts;
-//                 const auto src_ptr = reinterpret_cast<uint64_t>(rdma_x_src_idx);
-//                 const auto dst_ptr = reinterpret_cast<uint64_t>(rdma_recv_x) +
-//                                      dst_expert_local_idx * num_ranks * num_max_dispatch_tokens_per_rank * num_bytes_per_msg +
-//                                      rank * num_max_dispatch_tokens_per_rank * num_bytes_per_msg +
-//                                      slot_idx * num_bytes_per_msg;
-//                 const auto dst_p2p_ptr = nvshmemi_get_p2p_ptr(dst_ptr, rank, dst_rank);
-//                 if (dst_p2p_ptr == 0) {
-//                     nvshmemi_ibgda_put_nbi_warp(dst_ptr, src_ptr, num_bytes_per_msg, dst_rank, dst_expert_local_idx, lane_id, slot_idx);
-//                 } else {
-//                     // NOTES: only 2 load iterations for 7K hidden with 8 unrolls
-//                     const auto* src_int4_ptr = reinterpret_cast<const int4*>(src_ptr);
-//                     const auto* dst_int4_ptr = reinterpret_cast<int4*>(dst_p2p_ptr);
-//                     UNROLLED_WARP_COPY(8, lane_id, num_int4_per_msg, dst_int4_ptr, src_int4_ptr, ld_nc_global, st_na_global);
-//                 }
-//
-//                 // Increase counter after finishing
-//                 __syncwarp();
-//                 lane_id == 0 ? atomic_add_release_global(atomic_finish_counter_per_expert + dst_expert_idx, 1) : 0;
-//             }
-//         }
 //     } else if (warp_id == num_warps - 1) {
 //         EP_DEVICE_ASSERT(num_sms > 1);
 //         if (sm_id == 0) {
