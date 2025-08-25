e12ee8a4918201d957b92c0ea9a2897473474fc0
fzyzcjy <ch271828n@outlook.com>
2025-08-25 20:22:25 +0800

    constexpr

--- csrc/kernels/internode_ll_v2.cuh
+++ csrc/kernels/internode_ll_v2.cuh
@@ -301,12 +301,12 @@ dispatch_v2(void* packed_recv_x, void* packed_recv_x_scales,
 
     // FP8 staffs
     constexpr int kNumPerChannels = kUseNVFP4 ? 16 : 128;
-    const int num_scales = kHidden / kNumPerChannels;
+    constexpr int num_scales = kHidden / kNumPerChannels;
     constexpr size_t hidden_bytes =
         kUseNVFP4
             ? kHidden * sizeof(__nv_fp8_storage_t) / 2
             : kHidden * (kUseFP8 ? sizeof(__nv_fp8_storage_t) : sizeof(nv_bfloat16));
-    const size_t hidden_int4 = hidden_bytes / sizeof(int4);
+    constexpr size_t hidden_int4 = hidden_bytes / sizeof(int4);
 
     // Message package: index at source (int), 3 reserved int fields, hidden data, FP8 scales
     // NOTES: currently we have 3 reserved int fields for future use
@@ -315,8 +315,8 @@ dispatch_v2(void* packed_recv_x, void* packed_recv_x_scales,
         int32_t,
         std::conditional_t<kUseFP8, int2, int4>>;
     using rdma_x_scale_t = std::conditional_t<kUseNVFP4, uint8_t, float>;
-    const size_t num_bytes_per_msg = sizeof(int4) + ((kUseFP8 || kUseNVFP4) ? (hidden_bytes + num_scales * sizeof(rdma_x_scale_t)) : hidden_bytes);
-    const size_t num_int4_per_msg = num_bytes_per_msg / sizeof(int4);
+    constexpr size_t num_bytes_per_msg = sizeof(int4) + ((kUseFP8 || kUseNVFP4) ? (hidden_bytes + num_scales * sizeof(rdma_x_scale_t)) : hidden_bytes);
+    constexpr size_t num_int4_per_msg = num_bytes_per_msg / sizeof(int4);
     EP_DEVICE_ASSERT(num_bytes_per_msg % sizeof(int4) == 0);
 
     // Sending phase
