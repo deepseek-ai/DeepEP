0149aa4615ea6bcbee1740fbfc28b522b7bef8e1
fzyzcjy <ch271828n@outlook.com>
2025-08-25 21:03:52 +0800

    extract

--- csrc/kernels/internode_ll_v2.cuh
+++ csrc/kernels/internode_ll_v2.cuh
@@ -3,31 +3,10 @@
 #include "launch.cuh"
 #include "ibgda_device.cuh"
 
+#include "internode_ll_v2_inc.cuh"
+
 namespace deep_ep {
 namespace internode_ll {
-
-// NOTE extracted from `dispatch` body
-template <bool kUseFP8, bool kUseNVFP4, int kHidden>
-struct DispatchConstsTemplate {
-    // FP8 staffs
-    static constexpr int kNumPerChannels = kUseNVFP4 ? 16 : 128;
-    static constexpr int num_scales = kHidden / kNumPerChannels;
-    static constexpr size_t hidden_bytes =
-        kUseNVFP4
-            ? kHidden * sizeof(__nv_fp8_storage_t) / 2
-            : kHidden * (kUseFP8 ? sizeof(__nv_fp8_storage_t) : sizeof(nv_bfloat16));
-    static constexpr size_t hidden_int4 = hidden_bytes / sizeof(int4);
-
-    // Message package: index at source (int), 3 reserved int fields, hidden data, FP8 scales
-    // NOTES: currently we have 3 reserved int fields for future use
-    using vec_t = std::conditional_t<
-        kUseNVFP4,
-        int32_t,
-        std::conditional_t<kUseFP8, int2, int4>>;
-    using rdma_x_scale_t = std::conditional_t<kUseNVFP4, uint8_t, float>;
-    static constexpr size_t num_bytes_per_msg = sizeof(int4) + ((kUseFP8 || kUseNVFP4) ? (hidden_bytes + num_scales * sizeof(rdma_x_scale_t)) : hidden_bytes);
-    static constexpr size_t num_int4_per_msg = num_bytes_per_msg / sizeof(int4);
-}
 
 template <bool kUseFP8, bool kUseUE8M0, bool kUseNVFP4, int kHidden>
 __forceinline__ __device__ int dispatch_send(int local_thread_id) {
