09a779a01a5726f67f2a91ba1358d84d0e691d71
fzyzcjy <ch271828n@outlook.com>
2025-08-29 12:22:11 +0800

    more

--- csrc/deep_ep.cpp
+++ csrc/deep_ep.cpp
@@ -1291,14 +1291,26 @@ Buffer::low_latency_dispatch(bool enable_v2, const torch::Tensor& x, const torch
         recv_hook = [=]() { launcher(LOW_LATENCY_RECV_PHASE); };
 
     const auto returned_x = enable_v2
-        ? TODO(buffer.dispatch_rdma_recv_data_buffer)
+        // https://stackoverflow.com/questions/58631466/create-a-torchtensor-from-c-c-array-without-using-from-blob
+        // https://docs.pytorch.org/cppdocs/api/function_namespacetorch_1ac009244049812a3efdf4605d19c5e79b.html
+        ? torch::from_blob(
+            buffer.dispatch_rdma_recv_data_buffer,
+            // ref: LowLatencyLayout constructor `dispatch_recv_data_buffer_bytes`
+            {num_experts, num_max_dispatch_tokens_per_rank, num_bytes_per_dispatch_msg},
+            {num_max_dispatch_tokens_per_rank * num_bytes_per_dispatch_msg, num_bytes_per_dispatch_msg, 1},
+            torch::TensorOptions().dtype(torch::kUInt8).device(torch::kCUDA)
+        ).index({
+            Slice(),
+            Slice(),
+            Slice(sizeof(int4), sizeof(int4) + hidden / 2)
+        })
         : packed_recv_x;
     if (enable_v2) {
         EP_HOST_ASSERT(returned_x.dim() == 3);
         EP_HOST_ASSERT(returned_x.size(0) == num_local_experts);
         EP_HOST_ASSERT(returned_x.size(1) == num_ranks * num_max_dispatch_tokens_per_rank);
-        EP_HOST_ASSERT(returned_x.size(2) == num_ranks * hidden / NUM_ELEMS_PER_PACK);
-        EP_HOST_ASSERT(returned_x.dtype() == torch::kInt32);
+        EP_HOST_ASSERT(returned_x.size(2) == num_ranks * hidden / 2);
+        EP_HOST_ASSERT(returned_x.dtype() == torch::kUInt8);
     }
 
     // Return values
