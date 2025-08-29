14602ec75ea275817a858b026ee6d5e7320fe731
shifangx <shifangx@nvidia.com>
2025-08-28 22:32:51 -0700

    change physical layout to be (l, m/128, k/4, 128, 4)

--- tests/test_low_latency.py
+++ tests/test_low_latency.py
@@ -78,7 +78,7 @@ def test_main(num_tokens: int, hidden: int, num_experts: int, num_topk: int,
                             recv_x_scale_packed = packed_recv_x[1].clone().contiguous()
                             print(f"rank {rank}, num_times {num_times}, i: {i}, recv_x_scale_packed.shape:{recv_x_scale_packed.shape}, recv_x_scale_packed.dtype: {recv_x_scale_packed.dtype}")
                             recv_x_scale_view = recv_x_scale_packed.view(torch.int32)
-                            recv_x_scale_view = recv_x_scale_view.contiguous().view(num_local_experts, int(num_ranks * num_tokens) // 4, hidden // (16 * 4), 4)
+                            recv_x_scale_view = recv_x_scale_view.contiguous().view(num_local_experts, int(num_ranks * num_tokens) // 128, hidden // (16 * 4), 128)
                             recv_x_scale_view = recv_x_scale_view.permute(0, 1, 3, 2)
                             recv_x_scale_view = recv_x_scale_view.contiguous().view(num_local_experts, int(num_ranks * num_tokens), hidden // (16 * 4))
                             print(f"rank {rank}, num_times {num_times}, i: {i}, recv_x_scale_packed.shape:{recv_x_scale_packed.shape}, recv_x_scale_packed.dtype: {recv_x_scale_packed.dtype}, recv_x_scale_view.shape: {recv_x_scale_view.shape}, recv_x_scale_view.dtype: {recv_x_scale_view.dtype}, recv_x_scale_view: {recv_x_scale_view}")
