9176b414c9727fa8fd2156d30d38d3644e6aff27
fzyzcjy <ch271828n@outlook.com>
2025-08-29 15:08:05 +0800

    more

--- csrc/kernels/internode_ll_v2.cuh
+++ csrc/kernels/internode_ll_v2.cuh
@@ -267,7 +267,9 @@ __forceinline__ __device__ void dispatch_send(
                 // NOTE only first 4B of 16B has value, the other 12B is not needed
                 __syncwarp();
                 if (lane_id == 0) {
-                    if (subroutine_thread_id % 32 == 0) { printf("[R%d,S%d,T%d] st-token-signal token_idx=%d \n", rank, sm_id, subroutine_thread_id, token_idx); }
+                    if (subroutine_thread_id % 32 == 0) { printf("[R%d,S%d,T%d] st-token-signal START addr=%p delta_addr=%d token_idx=%d\n",
+                        rank, sm_id, subroutine_thread_id, src_src_idx, (int)((int64_t)dst_ptr - (int64_t)rdma_recv_x), token_idx); }
+
                     st_release_sys_global(reinterpret_cast<int*>(dst_p2p_ptr), -token_idx - 1);
                 }
             }
@@ -521,7 +523,8 @@ __forceinline__ __device__ void dispatch_recv(
 
             // Read signal + Copy source info
             if (lane_id == 0) {
-                if (subroutine_thread_id % 32 == 0) { printf("[R%d,S%d,T%d] ld-token-signal START token_idx=%d\n", rank, sm_id, subroutine_thread_id, token_idx); }
+                if (subroutine_thread_id % 32 == 0) { printf("[R%d,S%d,T%d] ld-token-signal START addr=%p delta_addr=%d token_idx=%d\n",
+                    rank, sm_id, subroutine_thread_id, src_src_idx, (int)((int64_t)src_src_idx - (int64_t)rdma_recv_x), token_idx); }
 
                 int recv_src_idx;
                 while ((recv_src_idx = ld_acquire_sys_global(src_src_idx)) == 0);
