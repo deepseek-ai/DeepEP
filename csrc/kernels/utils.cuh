48c6ea7c348768778ffd924a952977d52ea0d087
fzyzcjy <ch271828n@outlook.com>
2025-08-30 17:44:14 +0800

    Revert "temp revert unroll copy speedup"
    This reverts commit c58019666c5fc449d64ca7f3424a4478c55ba4ba.

--- csrc/kernels/utils.cuh
+++ csrc/kernels/utils.cuh
@@ -2,38 +2,6 @@
 
 #include "exception.cuh"
 
-// #define UNROLLED_WARP_COPY(UNROLL_FACTOR, LANE_ID, N, DST, SRC, LD_FUNC, ST_FUNC) \
-// { \
-//     constexpr int kLoopStride = 32 * (UNROLL_FACTOR); \
-//     typename std::remove_reference<decltype(LD_FUNC((SRC) + 0))>::type unrolled_values[(UNROLL_FACTOR)]; \
-//     auto __src = (SRC); \
-//     auto __dst = (DST); \
-//     for (int __i = (LANE_ID); __i < ((N) / kLoopStride) * kLoopStride; __i += kLoopStride) { \
-//         _Pragma("unroll") \
-//         for (int __j = 0; __j < (UNROLL_FACTOR); ++ __j) \
-//             unrolled_values[__j] = LD_FUNC(__src + __i + __j * 32); \
-//         _Pragma("unroll") \
-//         for (int __j = 0; __j < (UNROLL_FACTOR); ++ __j) \
-//             ST_FUNC(__dst + __i + __j * 32, unrolled_values[__j]); \
-//     } \
-//     { \
-//         int __i = ((N) / kLoopStride) * kLoopStride + (LANE_ID); \
-//         _Pragma("unroll") \
-//         for (int __j = 0; __j < (UNROLL_FACTOR); ++ __j) { \
-//             if (__i + __j * 32 < (N)) { \
-//                 unrolled_values[__j] = LD_FUNC(__src + __i + __j * 32); \
-//             } \
-//         } \
-//         _Pragma("unroll") \
-//         for (int __j = 0; __j < (UNROLL_FACTOR); ++ __j) { \
-//             if (__i + __j * 32 < (N)) { \
-//                 ST_FUNC(__dst + __i + __j * 32, unrolled_values[__j]); \
-//             } \
-//         } \
-//     } \
-// }
-
-// old
 #define UNROLLED_WARP_COPY(UNROLL_FACTOR, LANE_ID, N, DST, SRC, LD_FUNC, ST_FUNC) \
 { \
     constexpr int kLoopStride = 32 * (UNROLL_FACTOR); \
@@ -48,9 +16,41 @@
         for (int __j = 0; __j < (UNROLL_FACTOR); ++ __j) \
             ST_FUNC(__dst + __i + __j * 32, unrolled_values[__j]); \
     } \
-    for (int __i = ((N) / kLoopStride) * kLoopStride + (LANE_ID); __i < (N); __i += 32) \
-        ST_FUNC(__dst + __i, LD_FUNC(__src + __i)); \
+    { \
+        int __i = ((N) / kLoopStride) * kLoopStride + (LANE_ID); \
+        _Pragma("unroll") \
+        for (int __j = 0; __j < (UNROLL_FACTOR); ++ __j) { \
+            if (__i + __j * 32 < (N)) { \
+                unrolled_values[__j] = LD_FUNC(__src + __i + __j * 32); \
+            } \
+        } \
+        _Pragma("unroll") \
+        for (int __j = 0; __j < (UNROLL_FACTOR); ++ __j) { \
+            if (__i + __j * 32 < (N)) { \
+                ST_FUNC(__dst + __i + __j * 32, unrolled_values[__j]); \
+            } \
+        } \
+    } \
 }
+
+// old
+// #define UNROLLED_WARP_COPY(UNROLL_FACTOR, LANE_ID, N, DST, SRC, LD_FUNC, ST_FUNC) \
+// { \
+//     constexpr int kLoopStride = 32 * (UNROLL_FACTOR); \
+//     typename std::remove_reference<decltype(LD_FUNC((SRC) + 0))>::type unrolled_values[(UNROLL_FACTOR)]; \
+//     auto __src = (SRC); \
+//     auto __dst = (DST); \
+//     for (int __i = (LANE_ID); __i < ((N) / kLoopStride) * kLoopStride; __i += kLoopStride) { \
+//         _Pragma("unroll") \
+//         for (int __j = 0; __j < (UNROLL_FACTOR); ++ __j) \
+//             unrolled_values[__j] = LD_FUNC(__src + __i + __j * 32); \
+//         _Pragma("unroll") \
+//         for (int __j = 0; __j < (UNROLL_FACTOR); ++ __j) \
+//             ST_FUNC(__dst + __i + __j * 32, unrolled_values[__j]); \
+//     } \
+//     for (int __i = ((N) / kLoopStride) * kLoopStride + (LANE_ID); __i < (N); __i += 32) \
+//         ST_FUNC(__dst + __i, LD_FUNC(__src + __i)); \
+// }
 
 namespace deep_ep {
 
