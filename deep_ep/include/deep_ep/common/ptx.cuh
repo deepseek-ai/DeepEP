554e516ca4de31204d0af896d2746ffcfe7682ce
Emmett.Yan <emmett.yan@jaguarmicro.com>
2026-08-25 11:08:49 +0800

    fix: st.bulk support for size operand with 32-bit length is introduced in PTX ISA version 9.0

--- deep_ep/include/deep_ep/common/ptx.cuh
+++ deep_ep/include/deep_ep/common/ptx.cuh
@@ -96,7 +96,11 @@ __forceinline__ __device__ void st_bulk(void* smem_ptr) {
     if (elect_one_sync()) {
         asm volatile("st.bulk.weak.shared::cta [%0], %1, 0;\n" ::
                      "r"(static_cast<uint32_t>(__cvta_generic_to_shared(smem_ptr))),
+#if defined(__CUDACC_VER_MAJOR__) and __CUDACC_VER_MAJOR__ >= 13
                      "r"(kNumBytes)
+#else
+                     "l"(static_cast<uint64_t>(kNumBytes))
+#endif
                      : "memory");
     }
 #else
